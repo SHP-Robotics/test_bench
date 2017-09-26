1d7e947941bac73a0e4aba8a4afb2f991492204a
Andrew Zhu <mommothazaz123@gmail.com>
2017-09-26 10:38:58 -0700

    began coding

---
+++ zhu/src/program/Main.java
@@ -0,0 +1,8 @@
+package program;
+
+public class Main {
+	static Program p = new Program();
+	public static void main(String[] args) {
+		p.run();
+	}
+}
---
+++ zhu/src/program/Program.java
@@ -0,0 +1,13 @@
+package program;
+
+public class Program {
+
+	public void run() {
+		// TODO Auto-generated method stub
+		init();
+		while (running) {
+			loop();
+		}
+	}
+
+}
